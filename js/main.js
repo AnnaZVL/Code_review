0b708a5f62852000ede627b1587f323084c404c8
Anna Zadiraka <annazadiraka@gmail.com>
2024-04-02 22:20:35 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3.5,
+    spaceBetween: 22,
+  })
